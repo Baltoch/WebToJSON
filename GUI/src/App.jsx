b6ce86790de09c2bfd923a86a9d8b2a42a9a9411
Paul Valerie GOMA <paulvalerie.goma@edu.ece.fr>
2024-12-09 10:13:01 +0100

    add validate button component

---
+++ GUI/src/App.jsx
@@ -0,0 +1,14 @@
+// import Header from "./components/Header";
+import PromptArea from "./components/PromptArea"
+
+function App() {
+
+    return (
+        <>
+            {/* <Header/> */}
+            <PromptArea/>
+        </>
+    );
+}
+
+export default App
